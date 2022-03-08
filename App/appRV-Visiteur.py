34c7af8646002226be89845fc4e4a8fa885e7113
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-03-08 11:11:21 +0100

    V0.2.2

--- App/appRV-Visiteur.py
+++ App/appRV-Visiteur.py
@@ -125,4 +125,4 @@ def addEchantillonsOfferts( matricule , numRapport ) :
 
 
 if __name__ == '__main__' :
-    app.run( debug = True , host = '127.0.0.1' , port = 5000 )
+    app.run( debug = True , host = '172.20.38.12' , port = 5000 )
